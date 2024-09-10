6d0cd6af4d43c14a850dfe885f24a61074c42276
jiadil <jiadil@sfu.ca>
2024-09-09 21:23:46 -0700

    part 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Web Dev!!</h1>
-      <Labs />
-    </div>
+    <HashRouter> {/* sets up the base mechanism to navigate between multiple screens */}
+      <div>
+        <h1>Welcome to Web Dev!!</h1>
+        <Routes> {/* display an element only if the URL matches the pattern in the path attribute. */}
+          <Route path="/" element={<Navigate to="Labs" />} /> {/* default to Labs */}
+          <Route path="/Labs/*" element={<Labs />} /> {/* only http://localhost:3000/#/Labs display Labs */}
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
